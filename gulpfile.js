265d873a3035dffe178256f99c7e198bebd45c47
Masuk <mhmasuk.personal@gmail.com>
2017-07-18 12:56:05 +0600

    New gulp pakage added.

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+gulp.task('default', function(){
+    console.log("Hooray - you created a gulp task.");
+});
+
+gulp.task('html', function(){
+    console.log("Imagine something new.")
+});
+

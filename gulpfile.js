300b5b39350dd9f6feade89600674b217f90b562
Masuk <mhmasuk.personal@gmail.com>
2017-07-19 02:16:28 +0600

    Add responsive image and css style.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,3 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch');
-
-gulp.task('default', function(){
-    console.log("Hooray - you created a gulp task.");
-});
-
-gulp.task('html', function(){
-    console.log("Imagine something new.")
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
